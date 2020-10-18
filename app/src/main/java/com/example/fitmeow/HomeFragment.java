b5cc5b7dfea02f33ad1d39f00d9317062fab804e
Xiaocheng <rachelchen07216@gmail.com>
2020-10-17 21:51:04 -0400

    Setup navigation from homeFragment to profileFragment

--- app/src/main/java/com/example/fitmeow/HomeFragment.java
+++ app/src/main/java/com/example/fitmeow/HomeFragment.java
@@ -3,10 +3,14 @@ package com.example.fitmeow;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,12 +63,14 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, null);
+        Button button = (Button) view.findViewById(R.id.start_button);
+        button.setOnClickListener(this);
+        return view;
     }
 
     @Override
-    public void onClick(View v) {
-        //Navigate to the next fragment. -- Xiaocheng
-
+    public void onClick(View view) {
+        Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_profileFragment);
     }
 }
